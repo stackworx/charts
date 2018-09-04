2f026d0907213e12246f4fe16d4c9b70d4c135aa
cliedeman <ciaranliedeman@gmail.com>
2018-09-04 11:51:43 +0200

    Added timeout seconds to probes

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
+helm lint ./app/
 helm package ./app/
+
+helm lint ./postgresql-s3-backup/ --values ./postgresql-s3-backup/test.values.yaml
 helm package ./postgresql-s3-backup/
 
 
