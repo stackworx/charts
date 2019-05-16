a42b5bc3318040d621705dbc2118c75b13534e25
Ciaran Liedeman <ciaran@stackworx.io>
2019-05-16 15:25:19 +0200

    fixed lint issues

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,5 +6,7 @@ helm package ./app/
 helm lint ./postgresql-s3-backup/ --values ./postgresql-s3-backup/test.values.yaml
 helm package ./postgresql-s3-backup/
 
+helm lint ./oauth-server/
+helm package ./oauth-server/
 
 helm repo index .
